d5eb8d760ed11262a83b16b25e4e2d170cf01bff
Jason Woods <jason.woods@datatri.be>
2020-01-07 19:02:23 -0600

    Initial creation of Angular project and component

---
+++ src/app/custom-form/custom-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-form',
+  templateUrl: './custom-form.component.html',
+  styleUrls: ['./custom-form.component.css']
+})
+export class CustomFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
