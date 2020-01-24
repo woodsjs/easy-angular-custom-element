ca20ea6d6f5aaf87e1285db46eedd70564e06830
Jason Woods <jason.woods@datatri.be>
2020-01-24 05:58:16 -0600

    Updated package.json to add the prod:build script. Updated the script to remove the build process.

--- preview/build.sh
+++ preview/build.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-ng build custom-form --prod --output-hashing=none && cat ../dist/custom-form/runtime-es2015.js ../dist/custom-form/polyfills-es2015.js ../dist/custom-form/scripts.js ../dist/custom-form/main-es2015.js > customform.js && cp ../dist/custom-form/styles.css styles.css
+echo 'concatenating files'
+cat ./dist/custom-form/runtime-es2015.js \
+./dist/custom-form/polyfills-es2015.js \
+./dist/custom-form/scripts.js \
+./dist/custom-form/main-es2015.js > ./preview/customform.js && \
+cp ./dist/custom-form/styles.css ./preview/styles.css
